11192a8dd14ceb0ac709e13344b0bab344a05e0c
ajum <chemistric-05@mail.ru>
2019-10-11 14:44:21 +0100

    Add data models and a grid table in UI

--- src/main/java/com/zuhkle/MainView.java
+++ src/main/java/com/zuhkle/MainView.java
@@ -1,10 +1,15 @@
 package com.zuhkle;
 
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.server.PWA;
 import com.vaadin.flow.router.Route;
+import com.zuhkle.models.Customer;
+import com.zuhkle.models.CustomerService;
+
+import java.util.List;
 
 /**
  * The main view contains a button and a click listener.
@@ -12,10 +17,16 @@ import com.vaadin.flow.router.Route;
 @Route
 @PWA(name = "My Application", shortName = "My Application")
 public class MainView extends VerticalLayout {
+    private CustomerService service = CustomerService.getInstance();
+    private Grid<Customer> grid = new Grid<>(Customer.class);
 
     public MainView() {
-        Button button = new Button("Click me",
-                event -> Notification.show("Clicked!"));
-        add(button);
+        final VerticalLayout layout = new VerticalLayout();
+        layout.addComponentAsFirst(grid);
+
+        List<Customer> customers = service.findAll();
+        grid.setItems(customers);
+
+        add(layout);
     }
 }
---
+++ src/main/java/com/zuhkle/models/CustomerStatus.java
@@ -0,0 +1,5 @@
+package com.zuhkle.models;
+
+public enum CustomerStatus {
+    ImportedLead, NotContacted, Contacted, Customer, ClosedLost
+}
